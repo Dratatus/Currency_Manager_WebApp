4f7e5ebc9847da94ce78f33ea0076aab15dba44b
Daniel Partatus <daniel.paratatus@int.pl>
2022-12-17 15:07:42 +0100

    Removed old initial project

--- Projekt_asp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Projekt_asp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
