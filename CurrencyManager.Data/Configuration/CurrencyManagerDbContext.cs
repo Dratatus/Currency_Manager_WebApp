04a8e2237b1113ea176cacd62359e9d2be8afe11
Daniel Partatus <daniel.paratatus@int.pl>
2023-01-02 18:56:16 +0100

    Added EF Core

---
+++ CurrencyManager.Data/Configuration/CurrencyManagerDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CurrencyManager.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyManager.Data.Configutation
+{
+    public class CurrencyManagerDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CurrencyManager;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entityTypeBuilder =>
+            {
+                entityTypeBuilder.ToTable("Users");
+                entityTypeBuilder.HasKey(u => u.Id);
+                entityTypeBuilder.Property(u => u.Login);
+                entityTypeBuilder.Property(u => u.Password);
+                entityTypeBuilder.Property(u => u.Email);
+                entityTypeBuilder.Property(u => u.Balance);
+            });
+        }
+    }
+}
---
+++ CurrencyManager.Data/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyManager.Data.Entities
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Balance { get; set; }
+    }
+}
